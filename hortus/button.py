81a396747bc2412da031945f0e33e14c32dfe07f
Hakim Moumen <hakim.moumen@gmail.com>
2021-04-20 20:38:10 +0200

    rab

---
+++ hortus/button.py
@@ -0,0 +1,14 @@
+#-*- coding: utf-8 -*-
+import RPi.GPIO as GPIO 
+
+class button:
+
+	def __init__(self, gpio):
+		self.gpio = gpio
+        #set pin to be an input pin and set initial value to be pulled low (off)
+        GPIO.setup(self.gpio, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) 
+		#print('\t*init : Push button GPIO : {}'.format(self.gpio))
+		self.type = "push button"
+
+	def status(self):
+		return GPIO.input(self.gpio)
--- main.py
+++ main.py
@@ -16,33 +16,53 @@ tank_min = 130
 well_min = 250
 watering_hour = "02:43 PM"
 
+def water(channel):
+    print("watering in progress ...")
+
+def drain_tank(channel):
+    print("draining tank in progress ...")
+
+def fill(channel):
+    print("filling in progress ...")
+
+def drain_well(channel):
+    print("draining well in progress ...")
+
 def main():
+	#setup event on pin rising edge
+	GPIO.add_event_detect(button1.gpio,GPIO.RISING,callback=water) 
+	GPIO.add_event_detect(button2.gpio,GPIO.RISING,callback=drain_tank) 
+	GPIO.add_event_detect(button3.gpio,GPIO.RISING,callback=fill)
+	GPIO.add_event_detect(button4.gpio,GPIO.RISING,callback=drain_well)
+
 	now = datetime.datetime.now().strftime("%I:%M %p")
+	
 	if (now == watering_hour):
 		stop_time = time.time() + 60
 		relay1.on()
 		counter = 0
 		while(time.time() != stop_time):
 			counter += 1 
-			sys.stdout.write("\r" + "{}\t - Arrosage en cours...".format(datetime.datetime.now().strftime("%I:%M:%S %p")))
+			sys.stdout.write("\r" + "{}\t - arrosage en cours...".format(datetime.datetime.now().strftime("%I:%M:%S %p")))
 			sys.stdout.flush()
 			time.sleep(1)
 	else:
-		sys.stdout.write("\r" + "\t{} - En attente...".format(datetime.datetime.now().strftime("%I:%M:%S %p")))
+		sys.stdout.write("\r" + "\t{} - en attente...".format(datetime.datetime.now().strftime("%I:%M:%S %p")))
 		sys.stdout.flush()
-if __name__ == "__main__":
-	relay1 = relay(22)
-	relay2 = relay(23)
-	relay3 = relay(24)
-	relay4 = relay(25)
 
-	relay1.test()
-	relay2.test()
-	#ultrasonic1 = ultrasonic(14, 15)
+if __name__ == "__main__":
 	
-	#rain1 = rain(17)
-	#rain2 = rain(27)
+	relay1 = relay(22) 		#init pump relay : tank > garden
+	relay2 = relay(23) 		#init pump relay : tank > sewer
+	relay3 = relay(24) 		#init pump relay : well > tank
+	relay4 = relay(25) 		#init pump relay : well > egout
+
+	button1 = button(10)  	#init push button : tank > garden
+	button2 = button(11)	#init push button : tank > sewer 
+	button3 = button(12)	#init push button : well > tank
+	button4 = button(13)	#init push button : well > sewer
 	
 	while True :
 		main()
+
 GPIO.cleanup()
