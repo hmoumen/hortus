be6d03e52fa7a4bb82af9109832eec69e614cd7a
Hakim Moumen <hakim.moumen@gmail.com>
2021-04-20 20:53:28 +0200

    rab

--- hortus/button.py
+++ hortus/button.py
@@ -3,12 +3,12 @@ import RPi.GPIO as GPIO
 
 class button:
 
-	def __init__(self, gpio):
-		self.gpio = gpio
+    def __init__(self, gpio):
+        self.gpio = gpio
         #set pin to be an input pin and set initial value to be pulled low (off)
-        GPIO.setup(self.gpio, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) 
-		#print('\t*init : Push button GPIO : {}'.format(self.gpio))
-		self.type = "push button"
+        GPIO.setup(self.gpio, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        #print('\t*init : Push button GPIO : {}'.format(self.gpio))
+        self.type = "push button"
 
 	def status(self):
 		return GPIO.input(self.gpio)
