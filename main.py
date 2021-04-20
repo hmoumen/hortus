0d5124b77c9265abd691b8f99c40d593487d508c
Hakim Moumen <hakim.moumen@gmail.com>
2021-04-20 21:12:45 +0200

    rab

--- main.py
+++ main.py
@@ -54,15 +54,15 @@ def main():
 
 if __name__ == "__main__":
 	
-	relay1 = relay(22) 		#init pump relay : tank > garden
-	relay2 = relay(24) 		#init pump relay : tank > sewer
-	relay3 = relay(26) 		#init pump relay : well > tank
-	relay4 = relay(28) 		#init pump relay : well > egout
+	relay1 = relay(6)) 		#init pump relay : tank > garden
+	relay2 = relay(10) 		#init pump relay : tank > sewer
+	relay3 = relay(11) 		#init pump relay : well > tank
+	relay4 = relay(31) 		#init pump relay : well > egout
 
-	button1 = button(15)  	#init push button : tank > garden
-	button2 = button(16)	#init push button : tank > sewer 
-	button3 = button(18)	#init push button : well > tank
-	button4 = button(20)	#init push button : well > sewer
+	button1 = button(21)  	#init push button : tank > garden
+	button2 = button(22)	#init push button : tank > sewer 
+	button3 = button(23)	#init push button : well > tank
+	button4 = button(24)	#init push button : well > sewer
 	
 	while True :
 		main()
