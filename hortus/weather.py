8ebbe707aa34c29583d090411f08abf9c11ebfea
Hakim Moumen <hakim.moumen@gmail.com>
2021-04-20 21:24:22 +0200

    rab

--- hortus/weather.py
+++ hortus/weather.py
@@ -7,12 +7,14 @@ def weather():
 	r_weather = requests.get(url_weather)
 	data = r_weather.json()
 
-	daypartName = data['daypart'][0]['daypartName'][1]
-	narrative = data['daypart'][0]['narrative'][1]
-	precipchance = data['daypart'][0]['precipChance'][1]
-	temperature = data['daypart'][0]['temperature'][1]
+	daypartName = data['daypart'][1]['daypartName'][1]
+	narrative = data['daypart'][1]['narrative'][1]
+	precipchance = data['daypart'][1]['precipChance'][1]
+	temperature = data['daypart'][1]['temperature'][1]
+	
 	if precipchance < 60:
-		print("Arrosage recommandé")
+		print("recommanded watering")
+	
 	print("{} : {} °C - {} %".format(daypartName, temperature, precipchance))
 
 def weather_forcast():
