ab6bc825d0a7cf13b522923e3ee3b63fb0a3daa2
Hakim Moumen <hakim.moumen@gmail.com>
2021-04-22 12:51:41 +0200

    rab

--- hortus/dht.py
+++ hortus/dht.py
@@ -11,11 +11,11 @@ class dht:
     def humidity(self):
         humidity, temperature = dht.read_retry(22, self.gpio)
         if humidity is not None:
-            print('Humidity={1:0.1f}%'.format(humidity))
+            print('Humidity ={1:0.1f}%'.format(humidity))
 
     def temperature(self):
         humidity, temperature = dht.read_retry(22, self.gpio)
         if humidity is not None and temperature is not None:
-            print('Temperature={0:0.1f}°C'.format(temperature)
+            print('Temperature ={0:0.1f}°C'.format(temperature)
 
 
