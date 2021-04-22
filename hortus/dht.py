f6286f5eda7ee90842c2c7bfb22b38d840d209a2
Hakim Moumen <hakim.moumen@gmail.com>
2021-04-22 12:57:05 +0200

    rab

--- hortus/dht.py
+++ hortus/dht.py
@@ -3,16 +3,15 @@ import RPi.GPIO as GPIO
 import Adafruit_DHT
 
 class dht:
-
-	def __init__(self, gpio):
-		self.gpio = gpio
-		self.type = "dht"
+    def __init__(self, gpio):
+        self.gpio = gpio
+        self.type = "dht"
     
-    def humidity(sefl):
+    def humidity(self):
         humidity, temperature = Adafruit_DHT.read_retry(22, self.gpio)
         if humidity is not None:
             print('Humidity = {1:0.1f}%'.format(humidity))
-
+    
     def temperature(self):
         humidity, temperature = Adafruit_DHT.read_retry(22, self.gpio)
         if humidity is not None and temperature is not None:
