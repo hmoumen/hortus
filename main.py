88bae8d5127437381d975fdb4f3ec5cde7d7b4f5
Hakim Moumen <hakim.moumen@gmail.com>
2021-04-22 20:51:53 +0200

    rab

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ GPIO.setmode(GPIO.BCM)
 tank_min = 130
 well_min = 250
 watering_duration = 5
-watering_hour = "12:36 PM"
+watering_hour = "08:53 PM"
 
 def chrono():
 	stop_time = time.time() + watering_duration
