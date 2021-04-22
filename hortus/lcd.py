58c40b57e7e4397f048c8dbc33aaacbe5aefcfa6
Hakim Moumen <hakim.moumen@gmail.com>
2021-04-22 15:13:31 +0200

    rab

--- hortus/lcd.py
+++ hortus/lcd.py
@@ -45,7 +45,7 @@ def lcd(temp, humidity, precip):
     # Draw a black filled box to clear the image.
     draw.rectangle((0,0,width,height), outline=0, fill=0)
 
-    draw.text((x, top),       "Temperature : " + str(temp),  font=font, fill=255)
+    draw.text((x, top),       "T°C : " + str(temp) + "HR %: " + str(humidity) ,  font=font, fill=255)
     draw.text((x, top+8),     "Humidite : " + str(humidity), font=font, fill=255)
     draw.text((x, top+16),    "Precipitation : " + str(precip),  font=font, fill=255)
 
