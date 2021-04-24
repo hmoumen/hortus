dbce0431b258ff7046c12ae5bc54b2bf3f63e72b
Hakim Moumen <hakim.moumen@gmail.com>
2021-04-24 16:07:56 +0200

    rab

--- main.py
+++ main.py
@@ -22,12 +22,7 @@ watering_duration = 5
 whour = "04:08 PM"
 
 def water(channel):
-	if (relay1.status() == False):
-		relay1.on()
-		print("watering in progress ...") 
-	else:
-		relay1.off()
-		print("watering off")
+	relay1.water("manual",hc1.get_percdepth(), 0, 0)
 
 def drain_tank(channel):
 	if (relay2.status() == False):
@@ -66,7 +61,7 @@ if __name__ == "__main__":
 
 	hc1 = ultrasonic(14,15)
 
-	GPIO.add_event_detect(button1.gpio,GPIO.RISING,callback=relay1.water("manual",hc1.get_percdepth(), 0, 0), bouncetime=200) 
+	GPIO.add_event_detect(button1.gpio,GPIO.RISING,callback=water, bouncetime=200) 
 	GPIO.add_event_detect(button2.gpio,GPIO.RISING,callback=drain_tank, bouncetime=200) 
 	GPIO.add_event_detect(button3.gpio,GPIO.RISING,callback=fill, bouncetime=200)
 	GPIO.add_event_detect(button4.gpio,GPIO.RISING,callback=drain_well, bouncetime=200)
